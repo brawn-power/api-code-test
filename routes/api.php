fbd72135edd4bd0963ed6a00b058668f2590ae0e
huynhtuanhuy <huynhtuanhuy1996@gmail.com>
2024-04-11 01:28:31 +0700

    Uncomment auth middleware

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::group(['middleware' => 'auth:sanctum'], function () {
+Route::group(['middleware' => 'auth:sanctum'], function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
@@ -24,4 +24,4 @@ use Illuminate\Support\Facades\Route;
         Route::get('/', [WorkoutSessionController::class, 'index']);
         Route::post('/', [WorkoutSessionController::class, 'store']);
     });
-// });
+});
